b525cc0b2584dbd3dc284e861bc0d5fd26e8f5d0
Mamaalien <mariamakalokoh023@gmail.com>
2025-04-16 16:05:47 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "ING-SHERO BOT",
+  botOwner: 'ING-SHERO', // Your name
+  ownerNumber: '+23272433956', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
